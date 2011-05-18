3f66d69f3156ffb70fb83f47d799e0cf11330fc1
springyboing <github@accio.co.uk>
2011-05-18 08:57:09 -0700

    Added template prefix to enable us to exclude some less files from the compile phase.

--- src/groovy/com/github/grails/lesscss/Constants.groovy
+++ src/groovy/com/github/grails/lesscss/Constants.groovy
@@ -7,4 +7,5 @@ class Constants {
     public static final String LESS_SCRIPT_NAME = 'less-1.0.41'
     public static final String LESS_SCRIPT_FILE = "${LESS_SCRIPT_NAME}.js"
     public static final String LESS_SCRIPT_LOCATION = "/js/${LESS_SCRIPT_FILE}"
+    public static final String LESS_TEMPLATE_PREFIX = '_'
 }
